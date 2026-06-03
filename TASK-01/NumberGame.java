f9b3812418378a75176cc0f01c4a471fd0aabdd4
maniessh <v.maniessh@gmail.com>
2026-06-03 23:00:43 +0530

    nubergame project file create

---
+++ TASK-01/NumberGame.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGame{
+    Scanner sc=new Scanner(System.in);
+    Random rand=new Random();
+    Static int total=0;
+    public static void main(String[] args){
+        startGame();
+    }
+}
