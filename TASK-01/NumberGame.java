1bf39abded45ad3ccf2d26cc69c24669cd6e4776
maniessh <v.maniessh@gmail.com>
2026-06-04 23:06:55 +0530

    main function added

--- TASK-01/NumberGame.java
+++ TASK-01/NumberGame.java
@@ -1,10 +1,34 @@
 import java.util.Scanner;
 import java.util.Random;
 public class NumberGame{
+    static final int min =1;
+    static final int max=100;
+    static final int max_attemps=7;
     Scanner sc=new Scanner(System.in);
     Random rand=new Random();
-    Static int total=0;
-    public static void main(String[] args){
-        startGame();
+
+    static int total=0;
+    static int round_won=0;
+    static int roundNumber=0;
+
+
+public static void main (String[] args){
+    System.out.println("╔══════════════════════════════╗");
+        System.out.println("║    NUMBER GUESSING GAME      ║");
+        System.out.println("╚══════════════════════════════╝");
+    System.out.println("Guss a number between " +min + "and " +max);
+    System.out.println("You have " +max_attemps + " attempts to guess the number.");
+    boolean playAgain=true;
+    while(playAgain){
+        playRound();
+        playAgain=askPlayAgain();
     }
+    printFinalScore();
+    sc.close();
 }
+public static void playRound(){
+
+}
+
+}
+
